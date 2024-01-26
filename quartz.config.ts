d4135b015c93dcd95891b28516950ff127e963cb
Fernando Palafox <102826650+fernandopalafox@users.noreply.github.com>
2024-01-25 20:31:03 -0600

    Lowercase

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Futuro Inteligente",
+    pageTitle: "futuro inteligente",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
