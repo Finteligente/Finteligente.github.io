bf0fde4ace3209a53c096295512fa641bbf525f2
fernandopalafox <ferpalafoxe@gmail.com>
2024-01-25 18:07:09 -0600

    init

---
+++ content/index.md
@@ -0,0 +1,24 @@
+---
+title: Futuro Inteligente
+---
+
+<figure style="text-align: center;">
+  <img src="media/finteligente_logo_crop.png" alt="Quiribot" style="width:50%">
+</figure>
+
+**Futuro Inteligente** es un grupo de investigadores en inteligencia artificial (AI) que ofrece asesoramiento gratis para jovenes mexicanos interesados en dedicarse al desarollo de la inteligencia artificial. 
+
+Si eres alumno de secundaria, preparatoria, o licenciatura y te gustaria dedicarte a la inteligencia artificial, contactanos [aqui](mailto:name@example.com) para agendar una sesion.
+
+fernandopalafox@utexas.edu
+
+**Nuestro mision es simple:** inspirar jovenes Mexicanos a que resuelvan problemas de alto impacto a traves del desarrollo y la aplicacion de la inteligencia artificial. 
+
+**Con este fin, estos son nuestros objetivos:**
+1. Incrementar la participacion Mexicana en los niveles mas altos de investigacion fundamental sobre el desarollo de inteligencia artificial. 
+2. Fomentar el emprendimiento con herramientas basadas en la inteligencia artificial.
+Nuestra vision es un mundo donde Mexico sea un lider mundial en inteligencia artificial, la gente tenga no tenga miedo 
+
+Por lo tanto, ofrecemos asesoramiento gratis sobre como prepararse para una carrera en inteligencia artificial.
+
+Si eres un alumno de secundaria, preparatoria, o licenciatura y te gustaria dedicarte a la inteligencia artificial, contactanos aqui para agendar una sesion.
---
+++ quartz.config.ts
@@ -0,0 +1,77 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "Futuro Inteligente",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: {
+      provider: "plausible",
+    },
+    baseUrl: "futureintelligent.mx",
+    ignorePatterns: ["private", "templates", ".obsidian"],
+    defaultDateType: "published",
+    theme: {
+      typography: {
+        header: "Titillium Web",
+        body: "Titillium Web",
+        code: "IBM Plex Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#faf8f8",
+          lightgray: "#e5e5e5",
+          gray: "#b8b8b8",
+          darkgray: "#4e4e4e",
+          dark: "#2b2b2b",
+          secondary: "#45474B",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+        },
+        darkMode: {
+          light: "#161618",
+          lightgray: "#393639",
+          gray: "#646464",
+          darkgray: "	#F0ECE5",
+          dark: "#FFFFFF",
+          secondary: "#FFFFFF",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.TableOfContents(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "filesystem"], // you can add 'git' here for last modified from Git but this makes the build slower
+      }),
+      Plugin.SyntaxHighlighting(),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.Description(),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources({ fontOrigin: "googleFonts" }),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.NotFoundPage(),
+    ],
+  },
+}
+
+export default config
